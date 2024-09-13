ed64fcdafac6556717c8f04930e7f9a40cc1f71e
Azorlogh <bott.alix@gmail.com>
2024-09-13 17:10:09 +0200

    add grouping algo + improve equality checks

--- selo/src/traits/inside_equal.rs
+++ selo/src/traits/inside_equal.rs
@@ -1,14 +1,20 @@
-use crate::{MultiRing, Point, Polygon, Ring};
+use crate::{MultiPolygon, MultiRing, Point, Polygon, Ring};
 
 use super::IterPoints;
 
 /// Checks whether the set of points that these primitives represents are equal.
-/// Winding is not considered.
+/// Opposite windings are considered different.
 pub trait InsideEqual {
+    type Epsilon;
+
     fn inside_eq(&self, other: &Self) -> bool;
+
+    fn inside_abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool;
 }
 
 impl<P: Point> InsideEqual for Ring<P> {
+    type Epsilon = P::S;
+
     #[inline]
     fn inside_eq(&self, other: &Self) -> bool {
         let len = self.points_open().len();
@@ -20,17 +26,27 @@ impl<P: Point> InsideEqual for Ring<P> {
                 .cycle()
                 .take(len)
                 .eq(other.iter_points().cycle().skip(i).take(len))
-        }) || (0..len).any(|i| {
-            self.iter_points().rev().cycle().take(len).eq(other
-                .iter_points()
+        })
+    }
+
+    fn inside_abs_diff_eq(&self, other: &Self, epsilon: P::S) -> bool {
+        let len = self.points_open().len();
+        if len != other.points_open().len() {
+            return false;
+        }
+        (0..len).any(|i| {
+            self.iter_points()
                 .cycle()
-                .skip(i)
-                .take(len))
+                .take(len)
+                .zip(other.iter_points().cycle().skip(i).take(len))
+                .all(|(a, b)| a.abs_diff_eq(b, epsilon))
         })
     }
 }
 
 impl<P: Point> InsideEqual for MultiRing<P> {
+    type Epsilon = P::S;
+
     #[inline]
     fn inside_eq(&self, other: &Self) -> bool {
         if self.0.len() != other.0.len() {
@@ -40,12 +56,54 @@ impl<P: Point> InsideEqual for MultiRing<P> {
             .iter()
             .all(|a| other.0.iter().any(|b| a.inside_eq(b)))
     }
+
+    fn inside_abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        if self.0.len() != other.0.len() {
+            return false;
+        }
+        self.0
+            .iter()
+            .all(|a| other.0.iter().any(|b| a.inside_abs_diff_eq(b, epsilon)))
+    }
 }
 
 impl<P: Point> InsideEqual for Polygon<P> {
+    type Epsilon = P::S;
+
     #[inline]
     fn inside_eq(&self, other: &Self) -> bool {
         self.exterior().inside_eq(other.exterior()) && self.interior().inside_eq(other.interior())
+    }
+    #[inline]
+    fn inside_abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        self.exterior()
+            .inside_abs_diff_eq(other.exterior(), epsilon)
+            && self
+                .interior()
+                .inside_abs_diff_eq(other.interior(), epsilon)
+    }
+}
+
+impl<P: Point> InsideEqual for MultiPolygon<P> {
+    type Epsilon = P::S;
+
+    #[inline]
+    fn inside_eq(&self, other: &Self) -> bool {
+        if self.0.len() != other.0.len() {
+            return false;
+        }
+        self.0
+            .iter()
+            .all(|a| other.0.iter().any(|b| a.inside_eq(b)))
+    }
+    #[inline]
+    fn inside_abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
+        if self.0.len() != other.0.len() {
+            return false;
+        }
+        self.0
+            .iter()
+            .all(|a| other.0.iter().any(|b| a.inside_abs_diff_eq(b, epsilon)))
     }
 }
 
@@ -61,7 +119,7 @@ mod inside_eq_trait {
             MultiRing::empty(),
         );
         let polygon_2 = Polygon::new(
-            Ring::new(vec![Vec2::Y, Vec2::X, Vec2::ZERO]),
+            Ring::new(vec![Vec2::X, Vec2::Y, Vec2::ZERO]),
             MultiRing::empty(),
         );
 
