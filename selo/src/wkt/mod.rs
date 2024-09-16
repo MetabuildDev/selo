cb23476c8d3ca30ff4da6f15d2e2b92c06d79b33
Azorlogh <bott.alix@gmail.com>
2024-09-16 17:04:05 +0200

    fix wkt parsing

--- selo/src/wkt/mod.rs
+++ selo/src/wkt/mod.rs
@@ -1,3 +1,5 @@
+use crate::SeloScalar;
+
 /// Provides serialization as/deserialization from WKT.
 /// These modules are meant to be used with serde's with field attribute.
 /// See: https://serde.rs/field-attrs.html#with
@@ -9,6 +11,7 @@ pub mod ring3_polygon {
 
     use crate::prelude::*;
     use serde::{de, ser, Deserialize, Deserializer, Serialize, Serializer};
+    use wkt::Wkt;
 
     pub fn serialize<S, P: Point3>(ring: &Ring<P>, s: S) -> Result<S::Ok, S::Error>
     where
@@ -30,46 +33,24 @@ pub mod ring3_polygon {
     pub fn deserialize<'de, D, P: Point3>(d: D) -> Result<Ring<P>, D::Error>
     where
         D: Deserializer<'de>,
-        P::S3: FromStr,
+        P::S3: FromStr + Default,
         <P::S3 as FromStr>::Err: Display,
     {
-        let wkt = String::deserialize(d)?.trim().to_uppercase();
+        let wkt = String::deserialize(d)?
+            .parse::<Wkt<P::S>>()
+            .map_err(|e| de::Error::custom(e))?;
 
-        if !wkt.starts_with("POLYGON Z ((") && !wkt.starts_with("LINESTRING Z (") {
-            return Err(de::Error::custom("invalid wkt"))?;
-        }
+        let Wkt::Polygon(polygon) = wkt else {
+            return Err(de::Error::custom("wrong wkt type"));
+        };
 
-        let start = wkt
-            .rfind('(')
-            .ok_or_else(|| de::Error::custom("invalid wkt"))?;
-        let stop = wkt
-            .find(')')
-            .ok_or_else(|| de::Error::custom("invalid wkt"))?;
-        let coords: Vec<P> = wkt[(start + 1)..stop]
-            .split(',')
-            .map(|coord| {
-                let mut axis = coord.trim().split(' ');
-                Ok(P::new(
-                    axis.next()
-                        .ok_or(de::Error::custom("missing axis"))?
-                        .trim()
-                        .parse::<P::S3>()
-                        .map_err(|e| de::Error::custom(e))?,
-                    axis.next()
-                        .ok_or(de::Error::custom("missing axis"))?
-                        .trim()
-                        .parse::<P::S3>()
-                        .map_err(|e| de::Error::custom(e))?,
-                    axis.next()
-                        .ok_or(de::Error::custom("missing axis"))?
-                        .trim()
-                        .parse::<P::S3>()
-                        .map_err(|e| de::Error::custom(e))?,
-                ))
-            })
-            .collect::<Result<_, D::Error>>()?;
-
-        Ok(Ring::new(coords))
+        let exterior = polygon
+            .0
+            .get(0)
+            .ok_or_else(|| de::Error::custom("missing exterior ring"))?;
+        Ok(Ring::new(
+            super::wkt_linestring_coords_3d(&exterior.0).map_err(de::Error::custom)?,
+        ))
     }
 }
 
@@ -80,6 +61,7 @@ pub mod ring2_polygon {
 
     use crate::prelude::*;
     use serde::{de, ser, Deserialize, Deserializer, Serialize, Serializer};
+    use wkt::Wkt;
 
     pub fn serialize<S, P: Point2>(ring: &Ring<P>, s: S) -> Result<S::Ok, S::Error>
     where
@@ -100,41 +82,22 @@ pub mod ring2_polygon {
     pub fn deserialize<'de, D, P: Point2>(d: D) -> Result<Ring<P>, D::Error>
     where
         D: Deserializer<'de>,
-        P::S: FromStr,
+        P::S: FromStr + Default,
         <P::S as FromStr>::Err: Display,
     {
-        let wkt = String::deserialize(d)?.trim().to_uppercase();
+        let wkt = String::deserialize(d)?
+            .parse::<Wkt<P::S>>()
+            .map_err(|e| de::Error::custom(e))?;
 
-        if !wkt.starts_with("POLYGON ((") && !wkt.starts_with("LINESTRING (") {
-            return Err(de::Error::custom("invalid wkt"))?;
-        }
+        let Wkt::Polygon(polygon) = wkt else {
+            return Err(de::Error::custom("wrong wkt type"));
+        };
 
-        let start = wkt
-            .rfind('(')
-            .ok_or_else(|| de::Error::custom("invalid wkt"))?;
-        let stop = wkt
-            .find(')')
-            .ok_or_else(|| de::Error::custom("invalid wkt"))?;
-        let coords: Vec<P> = wkt[(start + 1)..stop]
-            .split(',')
-            .map(|coord| {
-                let mut axis = coord.trim().split(' ');
-                Ok(P::new(
-                    axis.next()
-                        .ok_or(de::Error::custom("missing axis"))?
-                        .trim()
-                        .parse::<P::S>()
-                        .map_err(|e| de::Error::custom(e))?,
-                    axis.next()
-                        .ok_or(de::Error::custom("missing axis"))?
-                        .trim()
-                        .parse::<P::S>()
-                        .map_err(|e| de::Error::custom(e))?,
-                ))
-            })
-            .collect::<Result<_, D::Error>>()?;
-
-        Ok(Ring::new(coords))
+        let exterior = polygon
+            .0
+            .get(0)
+            .ok_or_else(|| de::Error::custom("missing exterior ring"))?;
+        Ok(Ring::new(super::wkt_linestring_coords_2d(&exterior.0)))
     }
 }
 
@@ -145,6 +108,7 @@ pub mod ring3_linestring {
 
     use crate::prelude::*;
     use serde::{de, ser, Deserialize, Deserializer, Serialize, Serializer};
+    use wkt::Wkt;
 
     pub fn serialize<S, P: Point3>(ring: &Ring<P>, s: S) -> Result<S::Ok, S::Error>
     where
@@ -166,46 +130,20 @@ pub mod ring3_linestring {
     pub fn deserialize<'de, D, P: Point3>(d: D) -> Result<Ring<P>, D::Error>
     where
         D: Deserializer<'de>,
-        P::S3: FromStr,
+        P::S3: FromStr + Default,
         <P::S3 as FromStr>::Err: Display,
     {
-        let wkt = String::deserialize(d)?.trim().to_uppercase();
+        let wkt = String::deserialize(d)?
+            .parse::<Wkt<P::S>>()
+            .map_err(|e| de::Error::custom(e))?;
 
-        if !wkt.starts_with("POLYGON Z ((") && !wkt.starts_with("LINESTRING Z (") {
-            return Err(de::Error::custom("invalid wkt"))?;
-        }
+        let Wkt::LineString(ls) = wkt else {
+            return Err(de::Error::custom("wrong wkt type"));
+        };
 
-        let start = wkt
-            .rfind('(')
-            .ok_or_else(|| de::Error::custom("invalid wkt"))?;
-        let stop = wkt
-            .find(')')
-            .ok_or_else(|| de::Error::custom("invalid wkt"))?;
-        let coords: Vec<P> = wkt[(start + 1)..stop]
-            .split(',')
-            .map(|coord| {
-                let mut axis = coord.trim().split(' ');
-                Ok(P::new(
-                    axis.next()
-                        .ok_or(de::Error::custom("missing axis"))?
-                        .trim()
-                        .parse::<P::S3>()
-                        .map_err(|e| de::Error::custom(e))?,
-                    axis.next()
-                        .ok_or(de::Error::custom("missing axis"))?
-                        .trim()
-                        .parse::<P::S3>()
-                        .map_err(|e| de::Error::custom(e))?,
-                    axis.next()
-                        .ok_or(de::Error::custom("missing axis"))?
-                        .trim()
-                        .parse::<P::S3>()
-                        .map_err(|e| de::Error::custom(e))?,
-                ))
-            })
-            .collect::<Result<_, D::Error>>()?;
-
-        Ok(Ring::new(coords))
+        Ok(Ring::new(
+            super::wkt_linestring_coords_3d(&ls.0).map_err(de::Error::custom)?,
+        ))
     }
 }
 
@@ -216,6 +154,7 @@ pub mod ring2_linestring {
 
     use crate::prelude::*;
     use serde::{de, ser, Deserialize, Deserializer, Serialize, Serializer};
+    use wkt::Wkt;
 
     pub fn serialize<S, P: Point2>(ring: &Ring<P>, s: S) -> Result<S::Ok, S::Error>
     where
@@ -236,40 +175,33 @@ pub mod ring2_linestring {
     pub fn deserialize<'de, D, P: Point2>(d: D) -> Result<Ring<P>, D::Error>
     where
         D: Deserializer<'de>,
-        P::S: FromStr,
+        P::S: FromStr + Default,
         <P::S as FromStr>::Err: Display,
     {
-        let wkt = String::deserialize(d)?.trim().to_uppercase();
+        let wkt = String::deserialize(d)?
+            .parse::<Wkt<P::S>>()
+            .map_err(|e| de::Error::custom(e))?;
 
-        if !wkt.starts_with("POLYGON ((") && !wkt.starts_with("LINESTRING (") {
-            return Err(de::Error::custom("invalid wkt"))?;
-        }
+        let Wkt::LineString(ls) = wkt else {
+            return Err(de::Error::custom("wrong wkt type"));
+        };
 
-        let start = wkt
-            .rfind('(')
-            .ok_or_else(|| de::Error::custom("invalid wkt"))?;
-        let stop = wkt
-            .find(')')
-            .ok_or_else(|| de::Error::custom("invalid wkt"))?;
-        let coords: Vec<P> = wkt[(start + 1)..stop]
-            .split(',')
-            .map(|coord| {
-                let mut axis = coord.trim().split(' ');
-                Ok(P::new(
-                    axis.next()
-                        .ok_or(de::Error::custom("missing axis"))?
-                        .trim()
-                        .parse::<P::S>()
-                        .map_err(|e| de::Error::custom(e))?,
-                    axis.next()
-                        .ok_or(de::Error::custom("missing axis"))?
-                        .trim()
-                        .parse::<P::S>()
-                        .map_err(|e| de::Error::custom(e))?,
-                ))
-            })
-            .collect::<Result<_, D::Error>>()?;
-
-        Ok(Ring::new(coords))
+        Ok(Ring::new(super::wkt_linestring_coords_2d(&ls.0)))
     }
+}
+
+fn wkt_linestring_coords_2d<S: SeloScalar>(ls: &[wkt::types::Coord<S>]) -> Vec<S::Point2> {
+    use crate::point::Point2;
+    ls.iter()
+        .map(|p| S::Point2::new(p.x, p.y))
+        .collect::<Vec<_>>()
+}
+
+fn wkt_linestring_coords_3d<S: SeloScalar>(
+    ls: &[wkt::types::Coord<S>],
+) -> Result<Vec<S::Point3>, &'static str> {
+    use crate::point::Point3;
+    ls.iter()
+        .map(|p| Ok(S::Point3::new(p.x, p.y, p.z.ok_or("missing z coord")?)))
+        .collect::<Result<Vec<_>, _>>()
 }
